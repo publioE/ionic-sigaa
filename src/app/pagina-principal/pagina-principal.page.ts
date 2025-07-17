32366a8ae859a0fc0c742e1a91fa43e45a6a7e3f
Marchellon <marcelomwebber@gmail.com>
2025-07-17 15:23:54 -0300

    ajustes finais, padronização do header e outras pontas de css

--- src/app/pagina-principal/pagina-principal.page.ts
+++ src/app/pagina-principal/pagina-principal.page.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonCard } from '@ionic/angular/standalone';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
